33e1c89d00e8a7b7eb29b77f17dbb96dc17abed7
Thao Tran <thao.tran0510@outlook.com>
2025-08-16 13:42:42 +0700

    test

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.util.Strings;
 
 public class ChromeTest {
 //    @Test
---
+++ src/test/java/browsers/FirefoxTest.java
@@ -0,0 +1,21 @@
+package browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirefoxTest {
+    @Test
+    void openBrowserWithHeadlessMode(){
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
+}
